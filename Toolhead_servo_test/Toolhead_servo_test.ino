f00d3d3b55bccf697915cfe5e1279e284f51e99c
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-05-15 15:17:04 +0200

    add automatic mode to servo test (not working yet)

--- Toolhead_servo_test/Toolhead_servo_test.ino
+++ Toolhead_servo_test/Toolhead_servo_test.ino
@@ -4,15 +4,41 @@ Servo toolhead;
 
 int pos = 0;    // variable to store the servo position
 
+/*Modes:
+  0: Manual over Serial Port
+  1: Automatic, going up and down*/
+const int mode = 0;
+
 void setup() {
   toolhead.attach(9);
   Serial.begin(9600);
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    pos = Serial.parseInt();
-    toolhead.write(pos);
+  if(mode==0){
+    manual_mode();
+  }
+  if(mode==1){
+    automatic_mode();
+  }
+}
+
+void automatic_mode() {
+  while (true) {
+    toolhead.write(180);
     delay(100);
+    toolhead.write(90);
+    Serial.println("automatic mode cycled");
+  }
+}
+
+void manual_mode() {
+  while (true) {
+    if(Serial.available() > 0){
+        pos = Serial.parseInt();
+        toolhead.write(pos);
+        Serial.println(pos);
+        delay(100);
+      }
   }
 }
