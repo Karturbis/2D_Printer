b41d2c514d622e372864501a8e8c32652be0c0c1
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-06-16 16:35:33 +0200

    start writing the move function, which takes the target position as input

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,11 +1,19 @@
+#include <AccelStepper.h>
+
 #include "config.h"
+
+
+bool STATUS_LED_TOP = false;
+bool STATUS_LED_MID = false;
+bool STATUS_LED_BOT = false;
+int[] position;
+
 
 void setup() {
     // Setup Pins:
 
         // Led Pins:
-        pinMode(RX_LED_PIN, OUTPUT);
-        pinMode(TX_LED_PIN, OUTPUT);
+        pinMode(TRX_LED_PIN, OUTPUT);
         pinMode(STATUS_LED_TOP_PIN, OUTPUT);
         pinMode(STATUS_LED_MID_PIN, OUTPUT);
         pinMode(STATUS_LED_BOT_PIN, OUTPUT);
@@ -18,24 +26,33 @@ void setup() {
         pinMode(MOTOR_A_EN_PIN, OUTPUT);
         pinMode(MOTOR_A_DIR_PIN, OUTPUT);
         pinMode(MOTOR_A_STEP_PIN, OUTPUT);
-        pinMode(MOTOR_A_CS_PIN, OUTPUT);
         pinMode(MOTOR_B_EN_PIN, OUTPUT);
         pinMode(MOTOR_B_DIR_PIN, OUTPUT);
         pinMode(MOTOR_B_STEP_PIN, OUTPUT);
-        pinMode(MOTOR_B_CS_PIN, OUTPUT);
 
         // Axis Endswitch Pins:
-        pinMode(X_AXIS_END_SWITCH_PIN, INPUT);
-        pinMode(Y_AXIS_END_SWITCH_PIN, OUTPUT);
+        pinMode(X_AXIS_END_SWITCH_0_PIN, INPUT);
+        pinMode(X_AXIS_END_SWITCH_1_PIN, INPUT);
+        pinMode(Y_AXIS_END_SWITCH_0_PIN, INPUT);
+        pinMode(Y_AXIS_END_SWITCH_1_PIN, INPUT);
 
         // Toolhead Pins:
         pinMode(SERVO_PIN, OUTPUT);
         pinMode(WORKLIGHT_PIN, OUTPUT);
-
   Serial.begin(BAUD_RATE);
 
 }
 
 void loop() {
 
+}
+
+void move(int[2] target_position) {
+  int steps = (int)(FULLSTEP_TO_MICROMETER_RATIO*micrometer);
+}
+
+int homeing() {
+  // drive to x-axis stop
+  // drive to y-axis stop
+  //if error return 1, else 0
 }
