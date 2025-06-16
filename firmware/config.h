7ed1cd314307983690e1d4455817e69c4b7bf348
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-06-16 16:32:11 +0200

    change pinout in config.h

--- firmware/config.h
+++ firmware/config.h
@@ -11,30 +11,36 @@
 // Hardware Configuration:
 
     // Led Pins:
-    #define RX_LED_PIN 1
-    #define TX_LED_PIN 2
-    #define STATUS_LED_TOP_PIN 3
-    #define STATUS_LED_MID_PIN 4
-    #define STATUS_LED_BOT_PIN 5
+    #define TRX_LED_PIN 1
+    #define STATUS_LED_TOP_PIN 2
+    #define STATUS_LED_MID_PIN 3
+    #define STATUS_LED_BOT_PIN 4
 
     // Button pins
-    #define HOMING_BUTTON_PIN 6
-    #define WORKLIGHT_BUTTON_PIN 7
+    #define HOMING_BUTTON_PIN 5
+    #define WORKLIGHT_BUTTON_PIN 6
 
     // Motors:
-    #define MOTOR_A_EN_PIN 8
-    #define MOTOR_A_DIR_PIN 9
-    #define MOTOR_A_STEP_PIN 10
-    #define MOTOR_A_CS_PIN 11
-    #define MOTOR_B_EN_PIN 12
-    #define MOTOR_B_DIR_PIN 13
-    #define MOTOR_B_STEP_PIN 14
-    #define MOTOR_B_CS_PIN 15
+    //Pins:
+    #define MOTOR_A_EN_PIN 7
+    #define MOTOR_A_DIR_PIN 8
+    #define MOTOR_A_STEP_PIN 9
+    #define MOTOR_B_EN_PIN 10
+    #define MOTOR_B_DIR_PIN 11
+    #define MOTOR_B_STEP_PIN 12
+    //other:
+    #define FULLSTEP_TO_MICROMETER_RATIO 1
+    #define MICROSTEPPING 16
+    #define MAX_ACCELERATION 3000
+    #define MAX_SPEED 14000
+    #define WORKING_SPEED 1000
 
     // Axis-Endswitches:
-    #define X_AXIS_END_SWITCH_PIN 16
-    #define Y_AXIS_END_SWITCH_PIN 17
+    #define X_AXIS_END_SWITCH_0_PIN 13
+    #define X_AXIS_END_SWITCH_1_PIN 14
+    #define Y_AXIS_END_SWITCH_0_PIN 15
+    #define Y_AXIS_END_SWITCH_1_PIN 16
 
     // Toolhead:
-    #define SERVO_PIN 18
-    #define WORKLIGHT_PIN 19
+    #define SERVO_PIN 17
+    #define WORKLIGHT_PIN 18
