73531af761027a0f3b3b63095fc9c3c7c9ca34c9
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-06-17 11:02:53 +0200

    start writing the connect and the move methods, add moveto method, so absolute and relative coordinates can be used

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,3 +1,7 @@
+/*
+  This is the firmware for the 2D Printer.
+ */
+
 #include <AccelStepper.h>
 
 #include "config.h"
@@ -47,12 +51,33 @@ void loop() {
 
 }
 
-void move(int[2] target_position) {
-  int steps = (int)(FULLSTEP_TO_MICROMETER_RATIO*micrometer);
+void moveto(int[2] target_position) {
+}
+
+void move(float direction, int micrometers) {
+  // direction in radians
+  int steps[2];
+  int delta_x = (int)sin(direction)*micrometers;
+  int delty_y = (int)cos(direction)*micrometers;
+  steps[0] = (delta_x + delty_y)*STEP_TO_MICROMETER_RATIO;
+  steps[1] = (delta_x - delta_y)*STEP_TO_MICROMETER_RATIO;
+  // move motor a steps[0] and motor b steps[1]
+
 }
 
 int homeing() {
-  // drive to x-axis stop
-  // drive to y-axis stop
+  // drive to x-axis stop using move
+  // drive to y-axis stop using move
   //if error return 1, else 0
+}
+
+int connect() {
+  Serial.println("2D_Printer" + SOFTWARE_VERSION);
+  reply = Serial.readlines();
+  if(reply == "connect"){
+    return 0;
+  }
+  else {
+    return 1;
+  }
 }
