fd5165553bbc1a3ee079f786c278a1cc159314a5
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-06-19 18:15:05 +0200

    implement communication with computer, add toolhead() functions, add test_motor() function, start implementing homing fix Bugs in the move() function

--- firmware/config.h
+++ firmware/config.h
@@ -10,13 +10,21 @@
 // Connection with the PC:
 
 #define BAUD_RATE 115200
+//protocol Symbols:
+#define TERMINATOR Q
+#define SEPERATOR S
+// commands: 
+#define HOMING H
+#define GO_TO G
+#define DISENGAGE_TOOLHEAD U
+#define ENGAGE_TOOLHEAF D
 
 
 // Hardware Configuration:
 
     // Led Pins:
     #define TRX_LED_PIN 1
-    #define STATUS_LED_TOP_PIN 2
+    #define STATUS_LED_TOP_PIN A5
     #define STATUS_LED_MID_PIN 3
     #define STATUS_LED_BOT_PIN 4
 
@@ -27,33 +35,33 @@
     // Motors:
     //Pins:
     #define MOTOR_A_EN_PIN 2
-    #define MOTOR_A_DIR_PIN 4
-    #define MOTOR_A_STEP_PIN 3
-    #define MOTOR_A_RX_PIN 6
-    #define MOTOR_A_TX_PIN 5
+    #define MOTOR_A_DIR_PIN 3
+    #define MOTOR_A_STEP_PIN 4
+    #define MOTOR_A_RX_PIN 5
+    #define MOTOR_A_TX_PIN 6
     #define MOTOR_B_EN_PIN 7
     #define MOTOR_B_DIR_PIN 9
     #define MOTOR_B_STEP_PIN 8
-    #define MOTOR_B_RX_PIN 11
-    #define MOTOR_B_TX_PIN 10
+    #define MOTOR_B_RX_PIN 10
+    #define MOTOR_B_TX_PIN 11
     //other:
     #define MAX_MOTOR_CURRENT 400 // milli Ampere
     #define STEPPER_BAUD_RATE 115200
-    #define FULLSTEP_TO_MICROMETER_RATIO 200
-    #define MICROSTEPPING 16
+    #define FULLSTEP_TO_MICROMETER_RATIO 0.05
+    #define MICROSTEPPING 1
     #define MAX_ACCELERATION 3000
     #define MAX_SPEED 14000
     #define WORKING_SPEED 1000
+    #define WORKING_SPEED_DELAY 1000
 
     // Axis-Endswitches:
-    #define X_AXIS_END_SWITCH_0_PIN 13
-    #define X_AXIS_END_SWITCH_1_PIN 14
-    #define Y_AXIS_END_SWITCH_0_PIN 15
-    #define Y_AXIS_END_SWITCH_1_PIN 16
+    #define X_AXIS_END_SWITCH_0_PIN A1
+    #define X_AXIS_END_SWITCH_1_PIN A0
+    #define Y_AXIS_END_SWITCH_0_PIN A2
+    #define Y_AXIS_END_SWITCH_1_PIN A3
 
     // Toolhead:
-    #define SERVO_PIN 12
-    #define WORKLIGHT_PIN 13
+    #define SERVO_PIN 13
 
     // calculated values:
     #define STEP_TO_MICROMETER_RATIO (FULLSTEP_TO_MICROMETER_RATIO*MICROSTEPPING)
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -34,16 +34,16 @@ void setup() {
         // Button Pins:
         pinMode(HOMING_BUTTON_PIN, INPUT);
         pinMode(WORKLIGHT_BUTTON_PIN, INPUT);
-
+        */
         // Axis Endswitch Pins:
         pinMode(X_AXIS_END_SWITCH_0_PIN, INPUT);
         pinMode(X_AXIS_END_SWITCH_1_PIN, INPUT);
         pinMode(Y_AXIS_END_SWITCH_0_PIN, INPUT);
         pinMode(Y_AXIS_END_SWITCH_1_PIN, INPUT);
-        */
+        
         // Toolhead Pins:
         pinMode(SERVO_PIN, OUTPUT);
-        //pinMode(WORKLIGHT_PIN, OUTPUT);
+        pinMode(STATUS_LED_TOP_PIN, OUTPUT);
 
         // Motor Setup:
         // init motr pins:
@@ -69,6 +69,8 @@ void setup() {
         driver_b.I_scale_analog(false); // Use internal voltage reference
         driver_a.rms_current(MAX_MOTOR_CURRENT);      // Set driver current in mA
         driver_b.rms_current(MAX_MOTOR_CURRENT);      // Set driver current in mA
+        driver_a.microsteps(MICROSTEPPING);     // set microstepping driver a
+        driver_b.microsteps(MICROSTEPPING);     // set microstepping driver b
         driver_a.toff(2);               // Enable driver in software
         driver_b.toff(2);               // Enable driver in software
 
@@ -76,11 +78,11 @@ void setup() {
         digitalWrite(MOTOR_B_EN_PIN, LOW);    // Enable driver in hardware
 
   Serial.begin(BAUD_RATE);
-  Serial.setTimeout(1);
+  Serial.setTimeout(100);
   //status_led_top = true;
   //digitalWrite(STATUS_LED_TOP_PIN, HIGH);
 
-  Serial.println(SOFTWARE_VERSION);
+  //Serial.println(SOFTWARE_VERSION);
   //String reply = Serial.readString();
 
   /*if(connection_error) {
@@ -97,13 +99,34 @@ void setup() {
 }
 
 void loop() {
-  move(M_PI, 1000);
-  Serial.println("99");
+  digitalWrite(STATUS_LED_TOP_PIN, HIGH);
+  while (!Serial.available()) {
+  }
+  digitalWrite(STATUS_LED_TOP_PIN, LOW);
   while (Serial.available() == 0) {}     //wait for data available
-  String teststr = Serial.readString();  //read until timeout
-  teststr.trim();
-  delay(500);
-  Serial.println(teststr);
+  String command = Serial.readStringUntil('Q');  //read until timeout
+  command.trim();
+  if(command.startsWith("G")){
+    Serial.print("Command: ");
+    Serial.println(command);
+    String direction = command.substring(1, command.indexOf('S')); // string until the seperator, ignoring first char
+    String distance = command.substring(command.indexOf('S')+1); // string until terminator, not include seperator
+    Serial.print("Direction: ");
+    Serial.println(direction);
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    move(direction.toFloat(), distance.toInt()); // go to
+  }
+  else if (command.startsWith("HOMING")) {
+  //homeing();
+  }
+  else if (command.startsWith("DISENGAGE_TOOLHEAD")) {
+    disengage_toolhead();
+  }
+  else if (command.startsWith("ENGAGE_TOOLHEAD")) {
+    engage_toolhead();
+  }
+  Serial.println(0);
 }
 
 void moveto(int target_position[2]) {
@@ -121,20 +144,27 @@ void move(float direction, int micrometers) {
   int steps[2];
   int pos_a = 0;
   int pos_b = 0;
-  long delta_x = (long)sin(direction)*micrometers;
-  long delta_y = (long)cos(direction)*micrometers;
-  steps[0] = (delta_x + delta_y)*STEP_TO_MICROMETER_RATIO;
-  steps[1] = (delta_x - delta_y)*STEP_TO_MICROMETER_RATIO;
+  long delta_x = (long)(sin(direction)*micrometers);
+  long delta_y = (long)(cos(direction)*micrometers);
+  steps[0] = (int)(delta_x + delta_y)*STEP_TO_MICROMETER_RATIO;
+  steps[1] = (int)(delta_x - delta_y)*STEP_TO_MICROMETER_RATIO;
   stepper_a.move(steps[0]);
   stepper_b.move(steps[1]);
+  Serial.println("-----------------------------------");
+  Serial.println("######## Start moving #############");
+  Serial.println("-----------------------------------");
+  Serial.print("Delty_X: ");
+  Serial.println(delta_x);
+  Serial.print("Delty_y: ");
+  Serial.println(delta_y);
   Serial.println("About to start the while loop ...");
   Serial.print("Steps[0]: ");
   Serial.println(steps[0]);
   Serial.print("Steps[1]: ");
   Serial.println(steps[1]);
-  while (abs(pos_a) <= abs(steps[0]) && abs(pos_b) <= abs(steps[1])) {
+  while (abs(pos_a) < abs(steps[0]) || abs(pos_b) < abs(steps[1])) {
     Serial.println("Started While loop ...");
-    if(pos_a < steps[0]) {
+    if(pos_a <= steps[0]) {
       digitalWrite(MOTOR_A_DIR_PIN, LOW);
       digitalWrite(MOTOR_A_STEP_PIN, HIGH);
       pos_a ++;
@@ -149,12 +179,12 @@ void move(float direction, int micrometers) {
       digitalWrite(MOTOR_B_STEP_PIN, HIGH);
       pos_b ++;
     }
-    else if(pos_b > steps[0]) {
+    else if(pos_b > steps[1]) {
       digitalWrite(MOTOR_B_DIR_PIN, HIGH);
       digitalWrite(MOTOR_B_STEP_PIN, HIGH);
       pos_b --;
     }
-    delay(100);
+    delayMicroseconds(WORKING_SPEED_DELAY);
     digitalWrite(MOTOR_A_STEP_PIN, LOW);
     digitalWrite(MOTOR_B_STEP_PIN, LOW);
     Serial.print("pos_a: ");
@@ -166,18 +196,54 @@ void move(float direction, int micrometers) {
 
 void homeing() {
   // drive to x-axis stop using move:
+  Serial.println("Start Homing ...");
+  Serial.println("Homing X-Axis ...");
   bool stop = digitalRead(X_AXIS_END_SWITCH_0_PIN);
   while(!stop) {
-    move(HALF_PI, INVERSE_STEP_TO_MICROMETER_RATIO); // move one step
+    move(3*HALF_PI, 1); // move one step
     stop = digitalRead(X_AXIS_END_SWITCH_0_PIN);
   }
+  Serial.println("Hit the trigger, moving back");
+  // driving until the switch is not triggered anymore:
+  while (stop) {
+  move(HALF_PI, 1);
+  }
+  Serial.println("Finished Homing X-Axis");
   // drive to y-axis stop using move
 }
 
+void test_motor(){
+  while (true) {
+    digitalWrite(MOTOR_A_DIR_PIN, HIGH);
+    digitalWrite(MOTOR_B_DIR_PIN, HIGH);
+    Serial.println("Direction HIGH");
+    for (int i = 0; i < 500; i++) {
+        digitalWrite(MOTOR_A_STEP_PIN, !digitalRead(MOTOR_A_STEP_PIN));
+        digitalWrite(MOTOR_B_STEP_PIN, !digitalRead(MOTOR_B_STEP_PIN));
+        delay(1);
+    }
+    digitalWrite(MOTOR_A_DIR_PIN, LOW);
+    digitalWrite(MOTOR_B_DIR_PIN, LOW);
+    Serial.println("Direction LOW");
+    for (int i = 0; i < 500; i++) {
+        digitalWrite(MOTOR_A_STEP_PIN, !digitalRead(MOTOR_A_STEP_PIN));
+        digitalWrite(MOTOR_B_STEP_PIN, !digitalRead(MOTOR_B_STEP_PIN));
+        delay(1);
+    }
+  }
 
+}
 
 void send(String data) {
   digitalWrite(TRX_LED_PIN, HIGH);
   Serial.println(data);
   digitalWrite(TRX_LED_PIN, LOW);
+}
+
+void engage_toolhead(){
+  //not implemented yet
+}
+
+void disengage_toolhead(){
+  //no implemented yet
 }
