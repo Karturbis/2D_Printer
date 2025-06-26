dfee19c4e1df54d70616506f3773d16a1fd78c82
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-06-26 13:28:54 +0200

    rename Axis endswitch configuration, update the config

--- Axis-endswitches_configuration/config.h
+++ Axis-endswitches_configuration/config.h
@@ -5,13 +5,9 @@
 
 // general:
 
-#define SOFTWARE_VERSION "Beta"
+#define SOFTWARE_VERSION "0.1.2"
 
 // Connection with the PC:
-
-#define TOGGLE_X_AXIS false
-#define TOGGLE_Y_AXIS false
-#define TOGGLE_X_Y_AXIS false
 
 #define BAUD_RATE 115200
 //protocol Symbols:
@@ -22,13 +18,14 @@
 #define GO_TO "g"
 #define DISENGAGE_TOOLHEAD "u"  
 #define ENGAGE_TOOLHEAD "d"
+#define CHANGE_TOOL "c"
 
 
 // Hardware Configuration:
 
     //// Led Pins:
     //#define TRX_LED_PIN 1
-    //#define STATUS_LED_TOP_PIN A5
+    #define STATUS_LED_TOP_PIN A5
     //#define STATUS_LED_MID_PIN 3
     //#define STATUS_LED_BOT_PIN 4
     /*
@@ -59,13 +56,12 @@
     //other:
     #define MAX_MOTOR_CURRENT 400 // milli Ampere
     #define STEPPER_BAUD_RATE 115200
-    #define FULLSTEP_TO_MICROMETER_RATIO 0.05
+    #define FULLSTEP_TO_MICROMETER_RATIO 0.48
     #define MICROSTEPPING 1
     // function name of the used algorithm for step by step moving:
-   #define STEPS_ALGORITHM move_steps
-//   #define STEPS_ALGORITHM move_steps_accelstepper
-//   #define STEPS_ALGORITHM move_steps_diagonal_micros
-//   #define STEPS_ALGORITHM move_steps_diagonal_slope
+//    #define STEPS_ALGORITHM move_steps
+//   #define STEPS_ALGORITHM move_steps_linear_interpolation_time // not working at the moment (same symptom as move_steps)
+   #define STEPS_ALGORITHM move_steps_linear_interpolation_slope
 
     #define ACCELERATION 3000.0 // for use with AccelStepper
     #define MAX_SPEED 14000     // for use with AccelStepper
@@ -74,12 +70,13 @@
     // for x axis: WORKING_SPEED_DELAY >= 134
     // for y axis: WORKING_SPEED_DELAY >= 155
     // use WORKING_SPEED_DELAY >= 170 for safety
-    #define WORKING_SPEED_DELAY 2000
-    #define HOMING_SPEED_DELAY 4000
+    #define WORKING_SPEED_DELAY 250
+    #define HOMING_SPEED_DELAY 1200
     // homing offsets in 10 000 micrometer
-    #define HOMING_OFFSET_X -20
-    #define HOMING_OFFSET_Y 1
-    #define HIGH_DELAY 1
+    #define HOMING_OFFSET_X 0
+    #define HOMING_OFFSET_Y 0
+    #define HOMING_MOVEBACK 1000
+    #define HIGH_DELAY 10
 
     // Axis-Endswitches:
     #define X_AXIS_END_SWITCH_0_PIN A0
@@ -91,7 +88,7 @@
     #define SERVO_PIN 12
     #define SERVO_UP_POSITION 10
     #define SERVO_DOWN_POSITION 0
+    #define SERVO_CHANGE_TOOL_POSITION 80
 
     // calculated values:
     #define STEP_TO_MICROMETER_RATIO (FULLSTEP_TO_MICROMETER_RATIO/MICROSTEPPING)
-    #define INVERSE_STEP_TO_MICROMETER_RATIO (1*STEP_TO_MICROMETER_RATIO)
