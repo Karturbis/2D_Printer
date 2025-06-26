a405a58bcee99f748903259563258c14c9e7d365
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-06-26 13:32:40 +0200

    this time really fixing printer not noticing, if it runs into wall

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -444,7 +444,7 @@ uint8_t move_steps_linear_interpolation_slope(int steps[2], int working_speed_de
 }
 
 uint8_t check_collision() {
-  if(!digitalRead(Y_AXIS_END_SWITCH_0_PIN)){
+  if(!digitalRead(X_AXIS_END_SWITCH_0_PIN)){
     digitalWrite(MOTOR_A_STEP_PIN, LOW);
     digitalWrite(MOTOR_B_STEP_PIN, LOW);
     Serial.println(F("DEBUG:motor a LOW"));
@@ -465,7 +465,7 @@ uint8_t check_collision() {
     Serial.println(F("DEBUG:motor B LOW"));
     return 3;
   }
-  if(!digitalRead(X_AXIS_END_SWITCH_1_PIN)){
+  if(!digitalRead(Y_AXIS_END_SWITCH_1_PIN)){
     digitalWrite(MOTOR_A_STEP_PIN, LOW);
     digitalWrite(MOTOR_B_STEP_PIN, LOW);
     Serial.println(F("DEBUG:motor a LOW"));
